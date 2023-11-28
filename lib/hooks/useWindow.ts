7dc29eebd6b0b10bef8ac81713b35205a94a9e9a
kenny019 <ngkenny019@gmail.com>
2023-11-28 14:22:00 +0800

    fix: response give layout initial width

--- lib/hooks/useWindow.ts
+++ lib/hooks/useWindow.ts
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 
 function useWindow() {
   const [windowDimension, setWindowDimension] = useState({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
+    width: typeof window !== "undefined" ? window.innerWidth : 1280,
+    height: typeof window !== "undefined" ? window.innerHeight : 1280,
   });
 
   useEffect(() => {
