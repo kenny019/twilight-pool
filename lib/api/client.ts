7f9ef9b982f5efaa16ac887c88d7f26ecf316d8a
kenny019 <ngkenny019@gmail.com>
2024-02-11 19:11:59 +0800

    refactor: client and relayer apis

---
+++ lib/api/client.ts
@@ -0,0 +1,64 @@
+import wfetch from "../http";
+
+const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_ENDPOINT as string;
+
+async function sendTradeOrder(tradeData: string) {
+  const body = JSON.stringify({
+    jsonrpc: "2.0",
+    method: "CreateTraderOrder",
+    params: {
+      data: tradeData,
+    },
+    id: 1,
+  });
+
+  const { success, data, error } = await wfetch(CLIENT_URL)
+    .post({ body })
+    .json<Record<string, any>>();
+
+  if (!success) {
+    console.error(error);
+
+    return {};
+  }
+
+  console.log("success sent trader order", data);
+  return data;
+}
+
+async function cancelTradeOrder({
+  accountId,
+  uuid,
+  orderType,
+  orderStatus,
+}: {
+  accountId: string;
+  uuid: string;
+  orderType: string;
+  orderStatus: string;
+}) {
+  const body = JSON.stringify({
+    jsonrpc: "2.0",
+    method: "CancelTraderOrder",
+    params: {
+      account_id: accountId,
+      uuid,
+      order_type: orderType,
+      order_status: orderStatus,
+    },
+    id: 1,
+  });
+
+  const { success, data, error } = await wfetch(CLIENT_URL)
+    .post({ body })
+    .json<Record<string, any>>();
+
+  if (!success) {
+    console.error(error);
+    return {};
+  }
+
+  return data;
+}
+
+export { sendTradeOrder, cancelTradeOrder };
--- lib/api/relayer.ts
+++ lib/api/relayer.ts
@@ -1,13 +1,13 @@
 import wfetch from "../http";
 
-const RELAYER_URL = process.env.NEXT_PUBLIC_RELAYER_ENDPOINT as string;
+const RELAYER_URL = process.env.NEXT_PUBLIC_CLIENT_ENDPOINT as string;
 
-async function sendTradeOrder(tradeData: string) {
+async function queryLendOrder(lendData: string) {
   const body = JSON.stringify({
     jsonrpc: "2.0",
-    method: "CreateTraderOrder",
+    method: "QueryLendOrderZkos",
     params: {
-      data: tradeData,
+      data: lendData,
     },
     id: 1,
   });
@@ -18,11 +18,9 @@ async function sendTradeOrder(tradeData: string) {
 
   if (!success) {
     console.error(error);
-
     return {};
   }
 
-  console.log("success sent trader order", data);
   return data;
 }
 
@@ -48,61 +46,4 @@ async function queryTradeOrder(tradeData: string) {
   return data;
 }
 
-async function cancelTradeOrder({
-  accountId,
-  uuid,
-  orderType,
-  orderStatus,
-}: {
-  accountId: string;
-  uuid: string;
-  orderType: string;
-  orderStatus: string;
-}) {
-  const body = JSON.stringify({
-    jsonrpc: "2.0",
-    method: "CancelTraderOrder",
-    params: {
-      account_id: accountId,
-      uuid,
-      order_type: orderType,
-      order_status: orderStatus,
-    },
-    id: 1,
-  });
-
-  const { success, data, error } = await wfetch(RELAYER_URL)
-    .post({ body })
-    .json<Record<string, any>>();
-
-  if (!success) {
-    console.error(error);
-    return {};
-  }
-
-  return data;
-}
-
-async function queryLendOrder(lendData: string) {
-  const body = JSON.stringify({
-    jsonrpc: "2.0",
-    method: "QueryLendOrderZkos",
-    params: {
-      data: lendData,
-    },
-    id: 1,
-  });
-
-  const { success, data, error } = await wfetch(RELAYER_URL)
-    .post({ body })
-    .json<Record<string, any>>();
-
-  if (!success) {
-    console.error(error);
-    return {};
-  }
-
-  return data;
-}
-
-export { sendTradeOrder, queryLendOrder, queryTradeOrder, cancelTradeOrder };
+export { queryLendOrder, queryTradeOrder };
