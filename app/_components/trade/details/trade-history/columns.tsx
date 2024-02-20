23b7eb0a8c1911d0248fa15270b66145b92af9e9
kenny019 <ngkenny019@gmail.com>
2024-02-20 22:09:22 +0800

    fix: hash link in detail panel

--- app/_components/trade/details/trade-history/columns.tsx
+++ app/_components/trade/details/trade-history/columns.tsx
@@ -66,7 +66,10 @@ export const tradeHistoryColumns: ColumnDef<Trade, any>[] = [
     header: "Transaction Hash",
     cell: (row) => (
       <Button className="justify-end" asChild variant="link">
-        <Link href={`https://nyks.twilight-explorer.com/transaction/`}>
+        <Link
+          href={`https://nyks.twilight-explorer.com/transaction/${row.getValue()}`}
+          target="_blank"
+        >
           {row.getValue()}
         </Link>
       </Button>
