64229fc9549e4d318b60c00485a1a456656f330d
kenny019 <ngkenny019@gmail.com>
2026-02-20 14:20:33 +0800

    fix: build error

--- lib/utils/masterAccountQueue.ts
+++ lib/utils/masterAccountQueue.ts
@@ -39,7 +39,6 @@ type QueueEntry<T> = {
 };
 
 class MasterAccountQueue {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private readonly queue: QueueEntry<any>[] = [];
   private _busy = false;
 
