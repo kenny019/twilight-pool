188f0607bda07d0640c66b38e8fd52f1dbb8ee42
kenny019 <ngkenny019@gmail.com>
2026-03-23 01:48:37 +0800

    fix: correct Sentry DSN with public key

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://o4511088694984704.ingest.de.sentry.io/4511088722313296",
+  dsn: "https://f90f1ee4c4fed7a71073bc38e0f97742@o4511088694984704.ingest.de.sentry.io/4511088722313296",
   environment: process.env.NEXT_PUBLIC_TWILIGHT_NETWORK_TYPE || "development",
   tracesSampleRate: 0,
 });
